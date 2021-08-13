27af95f9ebb3a9a5a3b453bd084d804a570d9883
ali 2236 <ali.gh2236@gmail.com>
2021-08-14 00:08:23 +0430

    tailwindcss

--- components/ShowCard.tsx
+++ components/ShowCard.tsx
@@ -1,10 +1,12 @@
-import { Card } from "antd";
 import { Show } from "lib/Show";
 
 export default function ShowCard({ show }: { show: Show }) {
+    const url = `/images/${show.slug}.jpg`;
     return (
-        <Card title={show.name} extra={show.score}>
-            <img src={`/images/${show.slug}.jpg`} />
-        </Card>
+        <div className="bg-white shadow-md rounded-md">
+            <div>{show.name}</div>
+            <div><span>*</span>{show.score}/10</div>
+            <img></img>
+        </div>
     );
 };
